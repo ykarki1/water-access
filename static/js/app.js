41f76f8020a9d24f55dcfebdf5ecff7d35b1ba75
Nicole Spaar <spaarnicole@gmail.com>
2019-07-01 06:42:59 -0700

    attempted a few new ways to fix the "Verdicts" but no luck. Moving on for now

--- static/js/app.js
+++ static/js/app.js
@@ -272,7 +272,7 @@ function year() {
 
 
 //! Spaar
-// These are the funcs that make the claim clickable, and reveals its truthfullness .onclick()
+//* These are the funcs that make the claim clickable, and reveals the "Verdict" about it .onclick()
 //todo these should probably be refactored into just one case switcher or otherwise
 //
 function colorFill_0() {
@@ -347,9 +347,15 @@ function colorFill_4() {
     .classed('text-white', true)
   //d3.select(".carousel-control-next-icon")
 };
+//
 
 
 
+
+
+
+/*
+//* Remove all of the .onlick CSS mods applied to "Verdicts"
 function clearColors(SlideID) {
   d3.select(SlideID)
     .select("h6")
@@ -364,46 +370,214 @@ function clearColors(SlideID) {
     .classed('text-light', true)
     .classed('text-white', false)
 };
+*/
 
 
 
 
 
-//todo   1) figure out the currently .active carousel-indicator ID
 
-//todo   2) match that to the corresponding carousel-SlideVerdict-ID, then... (case switcheroo?)
 
-//todo   3) d3.classed('display', '___') for the appropriate carousel-SlideVerdict-ID, and all
-//todo      others are set to display:none; ----- maybe make all display:none; then activate just the
-//todo      one desired each event?
+//todo:::       1) Figure out the currently .active carousel-indicator ID
+
+//todo:::       2) Match that to the corresponding carousel-SlideVerdict-ID, then... (case switcheroo?)
+
+//todo:::       3) D3.classed('display', '___') for the appropriate carousel-SlideVerdict-ID, and all
+//todo          others are set to display:none; ----- maybe make all display:none; then activate just the
+//todo          one desired each event?
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! ///////////////////////////////////////////////////////////////////////////////////////////////////// !//
+//!                                                                                                       !//
+//!                                     Library of Failures: Claims                                       !//
+//!                                                                                                       !//
+//! ///////////////////////////////////////////////////////////////////////////////////////////////////// !//
+//--------------------------------------------------------------------------------------------------------//
+//!   Hopes:::     D3.js to return the slide ID if class in('.active').
+//!   Result:::    It executes too fast and returns the wrong value (Slide previous to the .active).
+//
+//? If there are no other options, I'll go this route with a mega hacky arrowCase switcheroo to add
+//? or subtract 1 to or from the slide's ID value. Would need to desable indicator functionality too.
+//
+//
+//* This "time.sleep()" method did not work, it still executes one slide behind the desired slide.
+//
+//* Calling this as a .onchange() within the carousel-indicators did not execute when class was changed.
+//
+/*
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    };
+  };
+};
+
+function one() {
+  sleep(0);
+};
+
+function two() {
+  let activeSlideObj = d3.select(".carousel-indicators").select(".active");
+  return activeSlideObj
+};
+
+function three(activeSlideObj) {
+  console.log("Active Carousel Slide ID: " + activeSlideObj._groups[0][0].attributes[2].nodeValue + "\n");
+  alert("Active Carousel Slide ID: " + activeSlideObj._groups[0][0].attributes[2].nodeValue);
+};
+
 function verdictExecute() {
-  //? get the currently .active slide
-
-  let test = d3.select(".carousel-indicators").select(".active");
-  alert(test);
-  //console.log("test")
-  //console.log(test);
-  //console.log("test._groups]")
-  //console.log(test._groups)
-  //console.log("test._groups[0]")
-  //console.log(test._groups[0])
-  //console.log("test._groups[0][0]")
-  //console.log(test._groups[0][0])
+  one();
+  var activeSlideObj = two();
+  three(activeSlideObj);
+};
+*/
+//--------------------------------------------------------------------------------------------------------//
 //
-  //console.log("test._groups[0][0].id")
-  //console.log(test._groups[0][0].id)
+//--------------------------------------------------------------------------------------------------------//
+//!   Hopes:::     Custom event listeners will watch and report on any class changes from null to
+//!                ".active". Processes for rendering the "Verdicts" could then be built out.
+//!   Result:::    I am not really sure where the hang up is yet after a large amount of debugging...
+//
+/*
+//* Function creation for newly, custom created listeners/handlers' implimentation.
+// The ID of the element to be watches, as well as the desired call back are passed in to create the new listener. The interval is set to every 10ms, and each time the element is checked for my defined states of class being either being null or active. If a change between those 2 states is detected, the callback is invoked.
+function addClassNameListener(elemId, callback) {
+    var elem = document.getElementById(elemId);
+    var lastClassName = elem.className;
+    window.setInterval( function() {
+       var className = elem.className;
+        if (className !== lastClassName) {
+            callback();
+            lastClassName = className;
+        }
+    },10);
+};
+//
+//* Creations for onclick listener/handler of element ID: "arrow-Prev"
+// Declarations and inits of event listeners of class changes (specifically the addition or removal of the "active" class which is manipulated by the carousel to indicate which slide is currently active/shown to the user).
+//
+//*     Slides[0]
+//
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-0").className = "active";
+};
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-0").className = "";
+};
+addClassNameListener("carousel-IndicatorID-0", function () { alert("changed"); });
 //
 //
+//*     Slides[1]
 //
-  //console.log("test._groups[0][0].attributes")
-  //console.log(test._groups[0][0].attributes)
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-1").className = "active";
+};
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-1").className = "";
+};
+addClassNameListener("carousel-IndicatorID-1", function () { alert("changed"); });
 //
-  //console.log("test._groups[0][0].attributes[2]")
-  //console.log(test._groups[0][0].attributes[2])
 //
-  //! Works, but it is pulling to early and thus is a slide delayed...could hackjob fix by nerfing
-  //! the "Prev" carousel navigator arrow as well as the indicators' functionality
-  console.log("test._groups[0][0].attributes[2].nodeValue")
-  console.log(test._groups[0][0].attributes[2].nodeValue)
-}
-
+//*     Slides[2]
+//
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-2").className = "active";
+};
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-2").className = "";
+};
+addClassNameListener("carousel-IndicatorID-2", function () { alert("changed"); });
+//
+//
+//*     Slides[3]
+//
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-3").className = "active";
+};
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-3").className = "";
+};
+addClassNameListener("carousel-IndicatorID-3", function () { alert("changed"); });
+//
+//
+//*     Slides[4]
+//
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-4").className = "active";
+};
+document.getElementById("arrow-Prev").onclick = function () {
+  document.getElementById("carousel-IndicatorID-4").className = "";
+};
+addClassNameListener("carousel-IndicatorID-4", function () { alert("changed"); });
+//
+//
+//* Creations for onclick listener/handler of element ID: "arrow-Next"
+// Declarations and inits of event listeners of class changes (specifically the addition or removal of the "active" class which is manipulated by the carousel to indicate which slide is currently active/shown to the user).
+//
+//*     Slides[0]
+//
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-0").className = "active";
+};
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-0").className = "";
+};
+addClassNameListener("carousel-IndicatorID-0", function () { alert("changed"); });
+//
+//
+//*     Slides[1]
+//
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-1").className = "active";
+};
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-1").className = "";
+};
+addClassNameListener("carousel-IndicatorID-1", function () { alert("changed"); });
+//
+//
+//*     Slides[2]
+//
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-2").className = "active";
+};
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-2").className = "";
+};
+addClassNameListener("carousel-IndicatorID-2", function () { alert("changed"); });
+//
+//
+//*     Slides[3]
+//
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-3").className = "active";
+};
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-3").className = "";
+};
+addClassNameListener("carousel-IndicatorID-3", function () { alert("changed"); });
+//
+//
+//*     Slides[4]
+//
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-4").className = "active";
+};
+document.getElementById("arrow-Next").onclick = function () {
+  document.getElementById("carousel-IndicatorID-4").className = "";
+};
+addClassNameListener("carousel-IndicatorID-4", function () { alert("changed"); });
+//
+*/
+//--------------------------------------------------------------------------------------------------------//
